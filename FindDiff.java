5919aac56e62f67b94ed3efa9d0136b5d3fb3321
Maharajan <115205544+Maharajan07@users.noreply.github.com>
2025-10-04 22:50:52 +0530

    Find the Difference

--- FindDiff.java
+++ FindDiff.java
@@ -43,3 +43,22 @@ class FindDiff {
 
 // Time Complexity: O(n²)
 // Space Complexity: O(n) -> for (used[])
+
+/* // Optimized soln
+
+class Solution {
+    public char findTheDifference(String s, String t) {
+        int sumS = 0;
+        int sumT = 0;
+        for (int i=0; i<s.length(); i++) {
+            sumS += s.charAt(i);
+        }
+        for (int i=0; i<t.length(); i++) {
+            sumT += t.charAt(i);
+        }
+        return (char) (sumT - sumS);
+    }
+}
+    
+// Time Complexity: O(n)
+// Space Complexity: O(1) */
