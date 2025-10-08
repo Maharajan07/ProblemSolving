0dde2f383204ba738b7dfbb785250abb36ba620b
Maharajan <115205544+Maharajan07@users.noreply.github.com>
2025-10-08 23:49:47 +0530

    Squares of Sorted Array

--- SquaresOfArray.java
+++ SquaresOfArray.java
@@ -17,7 +17,7 @@ Constraints:
 -104 <= nums[i] <= 104
 nums is sorted in non-decreasing order. */
 
-
+// Brute Force Solution
 class SquaresOfArray {
     public int[] sortedSquares(int[] nums) {
         for (int i=0; i<nums.length; i++) {
@@ -38,3 +38,33 @@ class SquaresOfArray {
 
 // Time Complexity: O(n²)
 // Space Complexity: O(1)
+
+
+
+/* // method -2 (Two Pointer)
+
+class Solution {
+    public int[] sortedSquares(int[] nums) {
+        int n = nums.length;
+        int[] res = new int[n];
+
+        int left = 0;
+        int right = n-1;
+        int idx = n-1;
+
+        while (left <= right) {
+            if (Math.abs(nums[left]) > Math.abs(nums[right])) {
+                res[idx--] = nums[left]*nums[left];
+                left++;
+            }
+            else {
+                res[idx--] = nums[right]*nums[right];
+                right--;
+            }
+        }
+        return res;
+    }
+}
+
+// Time Complexity: O(n)
+// Space Complexity: O(n) */
