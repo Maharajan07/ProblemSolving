9ccb57e7d5af5332c3d8f3824046bb5b3fbd38c4
Maharajan <115205544+Maharajan07@users.noreply.github.com>
2025-10-21 22:56:25 +0530

    Power of Four

---
+++ PowerOfFour.java
@@ -0,0 +1,31 @@
+/* 342. Power of Four
+
+Given an integer n, return true if it is a power of four. Otherwise, return false.
+
+An integer n is a power of four, if there exists an integer x such that n == 4x.
+
+Example 1:
+Input: n = 16
+Output: true
+
+Example 2:
+Input: n = 5
+Output: false
+
+Example 3:
+Input: n = 1
+Output: true */
+
+public class PowerOfFour {
+    public boolean isPowerOfFour(int n) {
+        if (n <= 0) return false;
+
+        while (n % 4 == 0) {
+            n /= 4;
+        }
+        return n == 1;
+    }
+}
+
+// Time Complexity: O(log₄ n)
+// Space Complexity: O(1)
