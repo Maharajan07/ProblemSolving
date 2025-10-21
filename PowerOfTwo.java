e60da70b024349e26762c512190471036590993a
Maharajan <115205544+Maharajan07@users.noreply.github.com>
2025-10-21 22:45:01 +0530

    Power of Two

--- PowerOfTwo.java
+++ PowerOfTwo.java
@@ -31,5 +31,17 @@ class PowerOfTwo {
     }
 }
 
+
+/*
+// method -2(Bitwise Trick)
+
+class Solution {
+    public boolean isPowerOfTwo(int n) {
+        return n > 0 && (n & (n-1)) == 0;
+    }
+} 
+*/
+
+
 // Time Complexity: O(1)
 // Space Complexity: O(1)
