15313670bf408a091175dd61a790f4da70acee94
Maharajan <115205544+Maharajan07@users.noreply.github.com>
2025-10-24 21:21:31 +0530

    Two Furthest Houses with Diff Colors

--- TwoFurthestHouses.java
+++ TwoFurthestHouses.java
@@ -9,16 +9,14 @@ The distance between the ith and jth houses is abs(i - j), where abs(x) is the a
 Example 1:
 Input: colors = [1,1,1,6,1,1,1]
 Output: 3
-Explanation: In the above image, color 1 is blue, and color 6 is red.
-The furthest two houses with different colors are house 0 and house 3.
+Explanation: The furthest two houses with different colors are house 0 and house 3.
 House 0 has color 1, and house 3 has color 6. The distance between them is abs(0 - 3) = 3.
 Note that houses 3 and 6 can also produce the optimal answer.
 
 Example 2:
 Input: colors = [1,8,3,8,3]
 Output: 4
-Explanation: In the above image, color 1 is blue, color 8 is yellow, and color 3 is green.
-The furthest two houses with different colors are house 0 and house 4.
+Explanation: The furthest two houses with different colors are house 0 and house 4.
 House 0 has color 1, and house 4 has color 3. The distance between them is abs(0 - 4) = 4.
 
 Example 3:
