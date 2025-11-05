b72073dcf2606c3c9e13846de69810ecb3cab791
Maharajan <115205544+Maharajan07@users.noreply.github.com>
2025-11-05 14:21:18 +0530

    Sum of Square Numbers

---
+++ SumOfSquares.java
@@ -0,0 +1,32 @@
+/* 633. Sum of Square Numbers
+
+Given a non-negative integer c, decide whether there're two integers a and b such that a2 + b2 = c.
+
+Example 1:
+Input: c = 5
+Output: true
+Explanation: 1 * 1 + 2 * 2 = 5
+
+Example 2:
+Input: c = 3
+Output: false */
+
+
+public class SumOfSquares {
+    public boolean judgeSquareSum(int c) {
+        int left = 0;
+        int right = (int) Math.sqrt(c);
+
+        while (left <= right) {
+            long sqrSum = (long) left*left + (long) right*right;
+
+            if (sqrSum == c) return true;
+            else if (sqrSum < c) left++;
+            else right--;
+        }
+        return false;
+    }
+}
+
+// Time Complexity: O(√c)
+// Space Complexity: O(1)
