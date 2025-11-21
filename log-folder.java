1b5ed7ff233303ec55239cfaa5b2a7add20e3cf8
Maharajan <115205544+Maharajan07@users.noreply.github.com>
2025-11-21 11:15:13 +0530

    Crawler Log Folder

--- log-folder.java
+++ log-folder.java
@@ -45,3 +45,25 @@ class Solution {
 
 // Time Complexity: O(n)
 // Space Somplexity: O(n)
+
+/* 
+// Integer counter (optimal)
+
+class Solution {
+    public int minOperations(String[] logs) {
+        int depth = 0;
+
+        for (String log:logs) {
+            if (log.equals("../")) {
+                if (depth != 0) depth--;
+            }
+            else if (!log.equals("./")) depth++;
+        }
+
+        return depth;
+    }
+}
+
+// Time Complexity: O(n)
+// Space Complexity: O(1)
+ */
