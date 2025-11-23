2db879dbd8e142db61355b7d741213cb74782c7b
Maharajan <115205544+Maharajan07@users.noreply.github.com>
2025-11-23 19:44:45 +0530

    Sum of Distances

---
+++ Sum-of-distance.java
@@ -0,0 +1,48 @@
+/* 2615. Sum of Distances
+
+You are given a 0-indexed integer array nums. There exists an array arr of length nums.length,
+where arr[i] is the sum of |i - j| over all j such that nums[j] == nums[i] and j != i. If there is no such j, set arr[i] to be 0.
+
+Return the array arr.
+
+Example 1:
+Input: nums = [1,3,1,1,2]
+Output: [5,0,3,4,0]
+Explanation: 
+When i = 0, nums[0] == nums[2] and nums[0] == nums[3]. Therefore, arr[0] = |0 - 2| + |0 - 3| = 5. 
+When i = 1, arr[1] = 0 because there is no other index with value 3.
+When i = 2, nums[2] == nums[0] and nums[2] == nums[3]. Therefore, arr[2] = |2 - 0| + |2 - 3| = 3. 
+When i = 3, nums[3] == nums[0] and nums[3] == nums[2]. Therefore, arr[3] = |3 - 0| + |3 - 2| = 4. 
+When i = 4, arr[4] = 0 because there is no other index with value 2. 
+
+Example 2:
+Input: nums = [0,5,3]
+Output: [0,0,0]
+Explanation: Since each element in nums is distinct, arr[i] = 0 for all i. */
+
+
+class Solution {
+    public long[] distance(int[] nums) {
+        List<Long> list = new ArrayList<>();
+        long sum = 0;
+
+        for (int i=0; i<nums.length; i++) {
+            for (int j=0; j<nums.length; j++) {
+                if (i != j && nums[i] == nums[j]) {
+                    sum += Math.abs(i - j);
+                }
+            }
+            list.add(sum);
+            sum = 0;
+        }
+
+        long[] arr = new long[list.size()];
+        for (int i=0; i<list.size(); i++) {
+            arr[i] = list.get(i);
+        }
+        return arr;
+    }
+}
+
+// Time Complexity: O(n²)
+// Space Complexity: O(n)
