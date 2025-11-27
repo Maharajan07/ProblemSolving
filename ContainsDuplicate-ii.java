7ed07a1a0d3d78b4eefc8f2d2777706ffd13eb0c
Maharajan <115205544+Maharajan07@users.noreply.github.com>
2025-11-27 11:40:59 +0530

    Contains Duplicate II

--- ContainsDuplicate-ii.java
+++ ContainsDuplicate-ii.java
@@ -31,5 +31,21 @@ class Solution {
     }
 }
 
+/* 
+class Solution {
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+        Set<Integer> set = new HashSet<>();
+
+        for (int i=0; i<nums.length; i++) {
+            if (i > k) {
+                set.remove(nums[i-k-1]);
+            }
+            if (!set.add(nums[i])) return true;
+        }
+        return false;
+    }
+}
+ */
+
 // Time Complexity: O(n)
 // Space Complexity: O(k)
