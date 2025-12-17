75e4a15cf1db710233f9e715fa4b1bb863d02cac
Maharajan <115205544+Maharajan07@users.noreply.github.com>
2025-12-17 10:21:33 +0530

    Search a 2D Matrix II

--- search-2d-matrix-ii.java
+++ search-2d-matrix-ii.java
@@ -13,7 +13,28 @@ Example 2:
 Input: matrix = [[1,4,7,11,15],[2,5,8,12,19],[3,6,9,16,22],[10,13,14,17,24],[18,21,23,26,30]], target = 20
 Output: false */
 
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int row = 0;
+        int col = matrix[0].length-1;
 
+        while (row < matrix.length && col >= 0) {
+            int cur = matrix[row][col];
+            if (cur == target) return true;
+            else {
+                if (cur < target) row++;
+                else col--;
+            }
+        }
+        return false;
+    }
+}
+
+// Time Complexity: O(m+n)
+// Space Complexity: O(1)
+
+
+/* 
 class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
         for (int i=0; i<matrix.length; i++) {
@@ -41,3 +62,4 @@ class Solution {
 
 // Time Complexity: O(m*log n)
 // Space Complexity: O(1)
+ */
