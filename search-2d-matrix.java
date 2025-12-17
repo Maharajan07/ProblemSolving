60b86e75972d795c1665124acd7c16146a2eebcc
Maharajan <115205544+Maharajan07@users.noreply.github.com>
2025-12-17 10:36:43 +0530

    Search a 2D Matrix

--- search-2d-matrix.java
+++ search-2d-matrix.java
@@ -32,3 +32,33 @@ class Solution {
 
 // Time Complexity: O(m*n)
 // Space Complexity: O(1)
+
+/* 
+// Better Approach: Binary Search
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int low = 0;
+        int high = matrix.length * matrix[0].length -1;
+
+        while (low <= high) {
+            int mid = (low + high)/2;
+            int row = mid / matrix[0].length;
+            int col = mid % matrix[0].length;
+
+            if (matrix[row][col] == target) {
+                return true;
+            }
+            else if (matrix[row][col] < target) {
+                low = mid + 1;
+            }
+            else {
+                high = mid - 1;
+            }
+        }
+        return false;
+    }
+}
+
+// Time Complexity: O(log(m*n))
+// Space Complexity: O(1)
+ */
