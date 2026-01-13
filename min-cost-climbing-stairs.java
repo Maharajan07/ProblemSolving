935f613b55bf32cf48a196302d28dceff5d17f01
Maharajan <115205544+Maharajan07@users.noreply.github.com>
2026-01-13 18:49:30 +0530

    Min cost climbing stairs

--- min-cost-climbing-stairs.java
+++ min-cost-climbing-stairs.java
@@ -28,6 +28,26 @@ The total cost is 6. */
 
 class Solution {
     public int minCostClimbingStairs(int[] cost) {
+        int n = cost.length;
+        int[] dp = new int[n+1];
+
+        dp[0] = 0;
+        dp[1] = 0;
+
+        for (int i=2; i<=n; i++) {
+            dp[i] = Math.min(dp[i-1] + cost[i-1], dp[i-2] + cost[i-2]);
+        }
+        return dp[n];
+    }
+}
+
+// Time Complexity: O(n)
+// Space Complexity: O(n)
+
+
+/* 
+class Solution {
+    public int minCostClimbingStairs(int[] cost) {
         int prev1 = 0;
         int prev2 = 0;
 
@@ -42,3 +62,4 @@ class Solution {
 
 // Time Complexity: O(n)
 // Space Complexity: O(1)
+ */
