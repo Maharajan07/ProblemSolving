9522e238c4cebdda5eab32a84c796a9004149683
Maharajan <115205544+Maharajan07@users.noreply.github.com>
2026-01-17 19:07:42 +0530

    House Robber II

--- House-robber-ii.java
+++ House-robber-ii.java
@@ -45,3 +45,31 @@ class Solution {
 
 // Time Complexity: O(n)
 // Space Complexity: O(1)
+
+
+/* 
+class Solution {
+    public int rob(int[] nums) {
+        int n = nums.length;
+        if (n == 1) return nums[0];
+        if (n == 2) return Math.max(nums[0], nums[1]);
+
+        return Math.max(robLinear(nums, 0, n-2), robLinear(nums, 1, n-1));
+    }
+
+    private int robLinear(int[] nums, int start, int end) {
+        int[] dp = new int[nums.length];
+
+        dp[start] = nums[start];
+        dp[start+1] = Math.max(nums[start], nums[start+1]);
+
+        for (int i=start+2; i<=end; i++) {
+            dp[i] = Math.max(dp[i-1], dp[i-2] + nums[i]);
+        }
+        return dp[end];
+    }
+}
+
+// Time Complexity: O(n)
+// Space Complexity: O(n)
+ */
