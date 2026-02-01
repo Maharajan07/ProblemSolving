6a40865794506ff57f83340d97c99d2f9b25227c
Maharajan <115205544+Maharajan07@users.noreply.github.com>
2026-02-01 22:48:44 +0530

    Rotate Array

--- rotate-array.java
+++ rotate-array.java
@@ -43,6 +43,30 @@ class Solution {
 // Time Complexity: O(n)
 // Space Complexity: O(1)
 
+
+/* 
+// alternative approach
+class Solution {
+    public void rotate(int[] nums, int k) {
+        int n = nums.length;
+        k = k % n;
+
+        int[] temp = new int[n];
+
+        for (int i=0; i<n; i++) {
+            temp[(i+k) % n] = nums[i];
+        }
+        for (int i=0; i<n; i++){
+            nums[i] = temp[i];
+        }
+    }
+}
+
+// Time Complexity: O(n)
+// Space Complexity: O(n)
+ */
+
+
 /* 
 // brute force approach
 
