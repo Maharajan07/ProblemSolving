e759d3e5f4da7b8d3c9077a17a89f35730f5efa1
Maharajan <115205544+Maharajan07@users.noreply.github.com>
2026-02-05 16:07:49 +0530

    Reverse Integer

--- rev-int.java
+++ rev-int.java
@@ -19,6 +19,23 @@ Output: 21 */
 
 class Solution {
     public int reverse(int x) {
+        long rev = 0;
+
+        while (x != 0) {
+            rev = (rev * 10) + x % 10;
+            x = x / 10;
+        }
+
+        if (rev <= Integer.MIN_VALUE || rev >= Integer.MAX_VALUE) return 0;
+
+        return (int) rev;
+    }
+}
+
+
+/* 
+class Solution {
+    public int reverse(int x) {
         int rev = 0;
 
         while (x != 0) {
@@ -32,23 +49,6 @@ class Solution {
         }
 
         return rev;
-    }
-}
-
-
-/* 
-class Solution {
-    public int reverse(int x) {
-        long rev = 0;
-
-        while (x != 0) {
-            rev = (rev * 10) + x % 10;
-            x = x / 10;
-        }
-
-        if (rev <= Integer.MIN_VALUE || rev >= Integer.MAX_VALUE) return 0;
-
-        return (int) rev;
     }
 }
  */
