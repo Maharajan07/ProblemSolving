b9de9695e9770c942bbff5804156a3e1b20e00df
Maharajan <115205544+Maharajan07@users.noreply.github.com>
2026-02-06 23:51:10 +0530

    Roman to Integer

--- Roman-to-integer.java
+++ Roman-to-integer.java
@@ -42,6 +42,37 @@ Explanation: M = 1000, CM = 900, XC = 90 and IV = 4. */
 
 class Solution {
     public int romanToInt(String s) {
+       int sum = 0;
+
+       for (int i=0; i<s.length(); i++) {
+            int cur = value(s.charAt(i));
+
+            if (i < s.length()-1 && cur < value(s.charAt(i+1))) {
+                sum -= cur;
+            }
+            else {
+                sum += cur;
+            }
+       } 
+       return sum;
+    }
+    private int value (char c) {
+        if (c == 'I') return 1;
+        if (c == 'V') return 5;
+        if (c == 'X') return 10;
+        if (c == 'L') return 50;
+        if (c == 'C') return 100;
+        if (c == 'D') return 500;
+        return 1000;
+    }
+}
+
+// Time Complexity: O(n)
+// Space Complexity: O(1)
+
+/* 
+class Solution {
+    public int romanToInt(String s) {
         Map<Character, Integer> map = new HashMap<>();
         map.put('I', 1);
         map.put('V', 5);
@@ -66,3 +97,4 @@ class Solution {
 
 // Time Complexity: O(n)
 // Space Complexity: O(1)
+*/
