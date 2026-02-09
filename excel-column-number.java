01947bda7828f4b18f3c7fefe703f227112bae31
Maharajan <115205544+Maharajan07@users.noreply.github.com>
2026-02-09 23:34:28 +0530

    Excel Sheet Column Number

---
+++ excel-column-number.java
@@ -0,0 +1,41 @@
+/* 171. Excel Sheet Column Number
+
+Given a string columnTitle that represents the column title as appears in an Excel sheet, return its corresponding column number.
+
+For example:
+A -> 1
+B -> 2
+C -> 3
+...
+Z -> 26
+AA -> 27
+AB -> 28 
+...
+
+Example 1:
+Input: columnTitle = "A"
+Output: 1
+
+Example 2:
+Input: columnTitle = "AB"
+Output: 28
+
+Example 3:
+Input: columnTitle = "ZY"
+Output: 701 */
+
+
+class Solution {
+    public int titleToNumber(String columnTitle) {
+        int res = 0;
+
+        for (int i=0; i<columnTitle.length(); i++) {
+            char ch = columnTitle.charAt(i);
+            res = res*26 + (ch - 'A' + 1);
+        }
+        return res;
+    }
+}
+
+// Time complexity: O(n)
+// Space complexity: O(1)
