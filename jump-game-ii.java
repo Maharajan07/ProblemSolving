96a36fb3f31de20a850f9550dd6f05b9d40838c7
Maharajan <115205544+Maharajan07@users.noreply.github.com>
2026-02-18 23:20:47 +0530

    Jump Game II

--- jump-game-ii.java
+++ jump-game-ii.java
@@ -38,3 +38,28 @@ class Solution {
 
 // Time Complexity: O(n)
 // Space Complexity: O(1)
+
+
+/* 
+// DP Solution
+class Solution {
+    public int jump(int[] nums) {
+        int n = nums.length;
+        int[] dp = new int[n];
+
+        for (int i=1; i<n; i++) {
+            dp[i] = Integer.MAX_VALUE;
+
+            for (int j=0; j<i; j++) {
+                if (j + nums[j] >= i) {
+                    dp[i] = Math.min(dp[i], dp[j] + 1);
+                }
+            }
+        }
+        return dp[n-1];
+    }
+}
+
+// Time Complexity: O(n^2)
+// Space Complexity: O(n)
+ */
