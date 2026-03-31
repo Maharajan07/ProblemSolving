dbdd7fad2456e9295690aafab5271162213b0a28
Maharajan <115205544+Maharajan07@users.noreply.github.com>
2026-03-31 23:47:44 +0530

    Numbers of Common Factors

--- common-factors.java
+++ common-factors.java
@@ -35,3 +35,22 @@ class Solution {
 
 // Time Complexity: O(log(gcd(a,b)) + sqrt(gcd(a,b)))
 // Space Complexity: O(1)
+
+
+/* 
+class Solution {
+    public int commonFactors(int a, int b) {
+        int n = Math.min(a, b);
+        int count = 0;
+
+        for (int i=1; i<=n; i++) {
+            if (a%i == 0 && b%i == 0) count++;
+        }
+
+        return count;
+    }
+}
+
+// Time Complexity: O(min(a,b))
+// Space Complexity: O(1)
+ */
