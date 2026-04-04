5129023eee2550e72078ba0e165fab5c99ab74a3
Maharajan <115205544+Maharajan07@users.noreply.github.com>
2026-04-04 23:47:08 +0530

    Valid Anagram

---
+++ valid-anagram.java
@@ -0,0 +1,34 @@
+/* 242. Valid Anagram
+
+Given two strings s and t, return true if t is an anagram of s, and false otherwise.
+
+Example 1:
+Input: s = "anagram", t = "nagaram"
+Output: true
+
+Example 2:
+Input: s = "rat", t = "car"
+Output: false */
+
+
+class Solution {
+    public boolean isAnagram(String s, String t) {
+
+        if (s.length() != t.length()) return false;
+
+        int[] count = new int[26];
+        for (int i=0; i<s.length(); i++) {
+            count[s.charAt(i) - 'a']++;
+            count[t.charAt(i) - 'a']--;
+        }
+
+        for (int n:count) {
+            if (n != 0) return false;
+        }
+
+        return true;
+    }
+}
+
+// Time Complexity: O(n)
+// Space Complexity: O(1)
